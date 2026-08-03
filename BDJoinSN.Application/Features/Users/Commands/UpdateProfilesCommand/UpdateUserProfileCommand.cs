c450bd61a5b09388553b1458d07d9e8271256803
Efrain <lancaihe148@gmail.com>
2026-08-03 15:35:04 -0700

    agrega cambios al updateprofilecommand

--- BDJoinSN.Application/Features/Users/Commands/UpdateProfilesCommand/UpdateUserProfileCommand.cs
+++ BDJoinSN.Application/Features/Users/Commands/UpdateProfilesCommand/UpdateUserProfileCommand.cs
@@ -11,6 +11,7 @@ namespace BDJoinSN.Application.Features.Users.Commands.UpdateProfilesCommand
         public string? LastName { get; set; }
         public string? Biography { get; set; }
         public string? Location { get; set; }
+        public string? ProfileImageUrl { get; set; }
         public DateTime? Birthday { get; set; }
     }
 }
